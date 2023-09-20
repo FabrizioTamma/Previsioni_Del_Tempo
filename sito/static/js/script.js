4dd09a824da935f561024fff3a215204313dd4f6
FabrizioTamma <Tamma.fabrizio@panettipitagora.edu.it>
2023-09-20 13:35:19 +0200

    incompleto con marker 1.5

--- sito/static/js/script.js
+++ sito/static/js/script.js
@@ -5,28 +5,36 @@ navigator.geolocation.getCurrentPosition(
 	function (event){
 		console.log("L'utente ha accettato")
 		console.log(event)
-		latitudine = event.cords.latitude
-		longitudine = event.cords.longitude
-		createMap()
-	
+		latitudine = event.coords.latitude
+		longitudine = event.coords.longitude
+		createMap(latitudine,longitudine)
 	
 },
 	
 	function (event){
 		console.log("L'utente non ha accettato")
 		console.log(event)
-		createMap()
+		createMap(latitudine,longitudine)
 		
 }
 )
 
-function createMap(){
+function createMap(latitudine, longitudine){
 	let map = L.map('map').setView([latitudine, longitudine], 16);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+let marker = L.marker([latitudine,longitudine]).addTo(map);
+
+map.on("click", function(event){
+	console.log(event)
+
+	marker.remove()
+	marker.L.marker([event.latling.lat, event.latling.lng]).addTo(map)
+
+})
+
 }
-
-
-let marker = L.marker([51.5, -0.09]).addTo(map);
